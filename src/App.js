9e42356855b223e6931b952cf3facc78c797ab0f
Baptiste Riffard <briffard@c1r1p2.hive.fi>
2022-09-22 15:43:09 +0300

    Update main branch | delete master branch

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Hero from './Component/Hero';
+import FullProfils from './Component/fullProfils';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+            })
+    }, []);
+
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+       {data && <Route exact path="/" element={<Hero data={data}/>} />}
+       {data && <Route path="/profil/:id" element={<FullProfils data= {data} />} />}
+      </Routes>
+    </Router>
+  );
+}
+export default App;
